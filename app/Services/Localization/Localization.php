d04d4d5d44d73f5cc338bd0e3f0ab12fc3f51231
sws-ava <smartwebstudio@ukr.net>
2022-05-19 15:16:19 +0300

    localization2

--- app/Services/Localization/Localization.php
+++ app/Services/Localization/Localization.php
@@ -9,6 +9,11 @@ class Localization
     {
 
         $locale = request()->segment(1, '');
+
+        // if ($locale == config('app.locale')) {
+        //     return '';
+        // }
+
         if ($locale && in_array($locale, config('app.locales'))) {
             return $locale;
         }
